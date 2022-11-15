17e930ca314e79192548846be8c75a9985119cb6
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-15 20:27:29 +0100

    Add BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
     return <>
-        Hallo Welt
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<>Welcome Home</>}/>
+                <Route path="cat" element={<>Meow!</>}/>
+            </Routes>
+            <br/>
+            <Link to="/">Go to Home</Link>{' '}
+            <Link to="cat">Go to Cat</Link>
+        </BrowserRouter>
     </>;
 }
 
